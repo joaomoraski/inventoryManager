20cb32f38c6062fbe04f582f28f93bc6ea76820a
João Moraski <joaomoraskilunkes@gmail.com>
2024-02-13 23:21:09 -0300

    Feat: inicializando o projeto em spring, criando o Dockerfile (que precisa ser revisado pq se pa ta errado), criando uma rota basica para testar a aplicação em spring.

--- reports-java/src/main/java/com/inventoryManager/inventoryManager/TesteController.java
+++ reports-java/src/main/java/com/inventoryManager/inventoryManager/TesteController.java
@@ -1,2 +1,12 @@
-package com.inventoryManager.inventoryManager;public class TesteController {
+package com.inventoryManager.inventoryManager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class TesteController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
